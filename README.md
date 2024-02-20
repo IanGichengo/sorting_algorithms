0524c1d1c115415dd93746a678a9c47396ab72b4
IanGichengo <iangatimu6@gmail.com>
2024-02-20 10:28:30 -0500

    sorting_algorithms

---
+++ README.md
@@ -0,0 +1,10 @@
+Sorting algorithm
+Big O notation
+Sorting algorithms animations
+15 sorting algorithms in 6 minutes (its a video)
+CS50 Algorithms explanation in detail by David Malan
+All about sorting algorithms
+At least four different sorting algorithms
+What is the Big O notation, and how to evaluate the time complexity of an algorithm
+How to select the best sorting algorithm for a given input
+What is a stable sorting algorithm
---
+++ sort.h
@@ -0,0 +1,25 @@
+#ifndef SORTING_H
+#define SORTING_H
+#include <stdio.h>
+#include <stddef.h>
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Prototypes */
+
+
+
+#endif
