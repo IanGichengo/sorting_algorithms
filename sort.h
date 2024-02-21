fbd1c74a2eb2153b7094a9e212df5ad90b24c7a2
IanGichengo <iangatimu6@gmail.com>
2024-02-21 14:45:51 -0500

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -29,5 +29,8 @@ void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+
+
 
 #endif
