9ae976faf2baa41eafa8801b5da40ceaecb76019
IanGichengo <iangatimu6@gmail.com>
2024-02-24 15:08:10 -0500

    sorting_algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,40 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
- *                  using the Selection sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
  */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
+/**
+ * selection_sort - Sorts an array of integers in ascending order using
+ *                  the selection sort algorithm
+ * @array: Pointer to the first element of the array
+ * @size: Size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
 	size_t i, j;
-	int temp, min_index;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
+		size_t min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
+			{
 				min_index = j;
+			}
 		}
-
 		if (min_index != i)
 		{
-			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-
+			swap(&array[min_index], &array[i]);
 			print_array(array, size);
 		}
 	}
